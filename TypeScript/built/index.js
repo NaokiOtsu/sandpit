58053162a2eb5c0af6b371cb77777b15989f7bda
NaokiOtsu <n.otsu1029@gmail.com>
2019-02-11 09:41:45 +0900

    update

---
+++ TypeScript/built/index.js
@@ -0,0 +1,9 @@
+var Octopus = /** @class */ (function () {
+    function Octopus(name) {
+        this.name = name;
+        this.numberOfLegs = 8;
+    }
+    return Octopus;
+}());
+var o = new Octopus("hgoe");
+console.log(o);
---
+++ TypeScript/src/index.ts
@@ -0,0 +1,7 @@
+class Octopus {
+  readonly numberOfLegs: number = 8;
+  constructor(readonly name: string) {}
+}
+
+const o = new Octopus("hgoe");
+console.log(o);
